1ca2ce2945feb617404ad0f18515af1ed71a4b98
omarcaballero <omarcm070400@gmail.com>
2025-04-17 20:37:33 -0500

    [fix]  Deleted paginated

--- src/services/client/ProductCLient.ts
+++ src/services/client/ProductCLient.ts
@@ -1,8 +1,8 @@
 import { ProductResponse } from "../../lib/types/types";
 import { fetchClient } from "../../lib/helper/fetchClient";
 
-export async function GetProductsClient(page: number = 1): Promise<ProductResponse> {
-    return fetchClient(`products/clients?page=${page}`, {
+export async function GetProductsClient(): Promise<ProductResponse> {
+    return fetchClient(`products`, {
         method: "GET"
     });
 }
